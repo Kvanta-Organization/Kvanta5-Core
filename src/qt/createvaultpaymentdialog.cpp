d64f9d50dbeb1f98005c1ba9fabb0c9fd4a8f0b9
Kvanta Mechanic <satooshi@bitcoin-ii.org>
2026-07-06 20:03:13 -0500

    Update createvaultpaymentdialog.cpp

--- src/qt/createvaultpaymentdialog.cpp
+++ src/qt/createvaultpaymentdialog.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2015-2026 The Bitcoin Core developers
+// Copyright (c) 2026 The Kvanta5 Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
 #include <qt/createvaultpaymentdialog.h>
 
 #include <qt/sendcoinsrecipient.h>
